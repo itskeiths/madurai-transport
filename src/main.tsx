6bda777c6ec64eddd8d6a7fabbdfbcae67553c81
itskeiths <keathareswar@gmail.com>
2026-03-04 11:51:11 +0530

    Removed Seed buses

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { seedBuses } from "./components/seedBuses";
+// import { seedBuses } from "./components/seedBuses";
 
 // Run seeding once before app renders
-const startApp = async () => {
-  await seedBuses();
+// const startApp = async () => {
+//   await seedBuses();
 
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-};
+//   createRoot(document.getElementById("root")!).render(
+//     <StrictMode>
+//       <App />
+//     </StrictMode>,
+//   );
+// };
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
 
-startApp();
+// startApp();
